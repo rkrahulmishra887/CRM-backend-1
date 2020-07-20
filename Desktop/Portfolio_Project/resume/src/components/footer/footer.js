630db174c0e1b70eb7543ee99f064636557315f1
rkrahulmishra887 <rkrahulmishra88714@gmail.com>
2020-07-21 02:49:14 +0530

    udated code

---
+++ Desktop/Portfolio_Project/resume/src/components/footer/footer.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+export default class Footer extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
+              {resumeData.socialLinks &&
+                resumeData.socialLinks.map((item) => {
+                  return (
+                    <li>
+                      <a href={item.url}>
+                        <i className={item.className} />
+                      </a>
+                    </li>
+                  );
+                })}
+            </ul>
+            <ul className="copyright">
+              <li>© Copyright 2020</li>
+              <li>
+                Design by{" "}
+                <a
+                  title="Styleshout"
+                  href="http://ec2-13-232-3-216.ap-south-1.compute.amazonaws.com/"
+                >
+                  Rahul Mishra
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
---
+++ Desktop/Portfolio_Project/resume/src/resumeData.js
@@ -0,0 +1,169 @@
+let resumeData = {
+  imagebaseurl: "https://techonoeticsclub.github.io/",
+  name: "Rahul Mishra",
+  role: "MERN Full Stack Developer and AWS Certified Cloud Practitioner   ",
+  linkedinId: "https://www.linkedin.com/in/rahul-mishra-172972152/",
+  emailId: "rkrahulmishra88714@gmail.com",
+  skypeid: "https://join.skype.com/invite/WIPWJ4xoPK7h",
+  roleDescription:
+    "   I  would like to learn about new technologies or simply play Cricket and Football in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "http://skype.com",
+      className: "fa fa-skype",
+    },
+  ],
+  aboutme:
+    "I have pursuing my B.tech in Electronics and Instrumentation and have been Intern at J.P Morgan, I'm a AWS Certified Cloud Practitioner, MERN Full Stack full Developer, ICSI | CNSS Certified Network Security Specialist, Certified File Transfer Professional, Virtual Intern at Microsoft.",
+  address:
+    "H/O Justice Mridula Mishra, Jagat Narayan Road, Kadamkuan, Patna- 800003, Bihar, India",
+  website: "http://ec2-13-232-3-216.ap-south-1.compute.amazonaws.com/",
+  education: [
+    {
+      UniversityName: "Haldia Institute of Technology",
+      specialization: "B.tech    in   Electronics and Instrumentation",
+      MonthOfPassing: "Pursuing",
+      YearOfPassing: "",
+      Achievements: "",
+    },
+  ],
+
+  achievements: [
+    {
+      CollegeName: "Indian Institute of Technology,Dhanbad",
+      Specialization: "Concetto",
+      onthOfPassing: "Oct",
+      yearOfPassing: "2018",
+      achievement: "1st Position in Robowar",
+    },
+  ],
+
+  work: [
+    {
+      CompanyName: "J.P Morgan",
+      specialization: "Software Developer Intern",
+      MonthOfLeaving: "Present",
+      YearOfLeaving: "2020",
+      Achievements: "",
+    },
+    {
+      CompanyName: "Microsoft",
+      specialization: "Software engineering Virtual Internship",
+      MonthOfLeaving: "July ,",
+      YearOfLeaving: "2020",
+      Achievements:
+        "Campus to Corporate: Business Foundation Skills, Campus to Corporate: work Ready Skills, Technical Engineer Skills ",
+    },
+    {
+      CompanyName: "Pro2ocol Limited.",
+      specialization: "Certified File Transfer Professional",
+      MonthOfLeaving: "Jun",
+      YearOfLeaving: "2020",
+      Achievements:
+        "◆Basic File Transfer Protocols>FTP, FTPS,SFTP,SCP ◆Advanced File Transfer Protocols >HTTP, HTTPS, WebDAV, SMTP, POP3, IMAP,NDM ◆Applicability Statement Protocols >AS1, AS2, AS3   ◆Accelerated File Transfer>Multi-Threading,TCP/IP,UDP/IP  ◆File Synchronization and Sharing  ◆At Rest Encryption ◆File Transfer Operations",
+    },
+    {
+      CompanyName: "International Cybersecurity Institute(ICSI)",
+      specialization: "ICSI | CNSS Certiﬁed Network Security Specialist",
+      MonthOfLeaving: "",
+      YearOfLeaving: "",
+      Achievements:
+        " ◆Network Security and Cyber Defence ◆Firewalls ◆Virtual Private Network ◆Assessing System Security ◆Operating System Hardening ◆Encryption",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "Python Programming",
+    },
+    {
+      skillname: "C++ programming",
+    },
+    {
+      skillname: "Linux",
+    },
+    {
+      skillname: "Nodejs",
+    },
+    {
+      skillname: "Expressjs",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "MongoDB",
+    },
+    {
+      skillname: "RestAPI",
+    },
+    {
+      skillname: "AWS",
+    },
+    {
+      skillname: "GIT",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "IoT based Drone For Military Apllications",
+      description:
+        "we are presenting a proposed system for IoT Based drone for militaryapplication. In this drone the most expensive feature is that the drone can fly, swim, run, using Pixhaw and Raspberry pi 3 using MQTT protocol and it is done by integrating with the help of various sensors,cameras into web application using MQTT and HTTP protocol. To develop and design, we are using Pixhawk and Raspberry pi 3 embedded board with python programming & MQTT protocol. Using this system can monitor and control the drone from anywhere through XB station. XB station is a solution for streaming video and controling UAV via internet (wifi, 3G/4G/5G cellular network). It includes: open hardware on drone (PX4 and ArduPilot) and software on ground . And it has various sensors like motion sensor to sense the existence of human, pitch sensor to make the 3D mapping, Inductive proximity sensor to detect landmines (metal), temperature sensor to sense the temperature and various gas sensors to detect hazardous gaseous in the environment. And this drones has extra feature is that it can draw a 3D mapping of specified location. Whenever detect, the raspberry pi will start publishing the data using the MQTT protocol and display on Server and also all the sensor data will publish on server. In this drone we have mounted ultrasonic sensor and xyro (MPU6050) with the help of this Sensor we are measure the area of this room and where the object is found after finding the object PIR sensor give the data of existance of human and also from pitch sensor we have making a 3D image of room where terrorist is hidden and all data will send to the server.",
+      imgurl: "images/portfolio/project.jpeg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description:
+        "Always available to help, incredibly proficient, hits deadlines without fail, hard working and trustworthy. In my experience, no project is too much trouble, and with Warren you really get a rare entity – someone that can simplify what can be a very complex industry at times.   I can recommend his services without hesitation.",
+      name: "Scott Petty, CTO, Enterprise",
+    },
+    {
+      description:
+        "Not only was Ian’s work on-time and to spec, he pays attention to improve the aesthetics of the site as well as the functionality. Throughout the entire process he is responsive, and willing to work through issues as they arise. It’s obvious he takes tremendous pride in his work, and I wouldn’t hesitate to recommend or work with him again.",
+      name: "Renaldo Webb Pet Plate",
+    },
+    {
+      description:
+        "I was looking for a Web professional who would create and evolve my website. Warren delivers exactly what I want. It’s always a pleasure working and seeing him. I enjoy meeting him as he lives locally and I always leave his place with a spring in my step and a smile on my face. I know he will keep improving my website and advising me in the best ways forward for years to come, with SEO and new features. I look forward to a great working relationship and friendship with him.",
+      name: "Flavius Fernandes, ERP Sirius",
+    },
+    {
+      description: "This is guy have potential to learn new Technology",
+      name: "Rahul Mishra",
+    },
+    {
+      description:
+        "We utilized Warren’s Project Management skills to oversee the smooth transition of our merger with Yahoo Sport. Our consumers gave us such positive feedback, we later returned to Warren, this time to design a new landing page for our corporate website. The results yet again were outstanding.",
+      name: "	Jaques Raynaud, VC, Eurosport Corporate",
+    },
+  ],
+};
+
+export default resumeData;
